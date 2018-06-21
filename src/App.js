77767e6143c3196f9a89e7b2b900765fee0da25c
github username <github email address>
2018-06-21 14:23:35 -0400

    some aesthetic changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux'
 import Login from './components/Login'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import HomeContainer from './components/HomeContainer'
--- src/components/Message.js
+++ src/components/Message.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Message = ({chat, user}) => (
     <li className={`chat ${user === chat.username ? "right" : "left"}`}>
         {user !== chat.username
-            && <img src={chat.img} alt={`${chat.username}'s profile pic`} />
-        }
+            && <img src={chat.img} alt={`${chat.username}'s profile pic`} />        }
         {chat.content}
     </li>
 );
